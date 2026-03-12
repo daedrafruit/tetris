d69f727a392c90226f554d8c21829ccf82ce1709
daedrafruit <160567370+daedrafruit@users.noreply.github.com>
2026-03-12 02:36:36 -0700

    draws I block

--- tile_map_layer.gd
+++ tile_map_layer.gd
@@ -1,9 +1,21 @@
 extends TileMapLayer
 
+var yellow = Vector2i(0,0)
+var sky = Vector2i(1,0)
+var blue = Vector2i(2,0)
+var red = Vector2i(3,0)
+var green = Vector2i(4,0)
+var orange = Vector2i(5,0)
+var purple = Vector2i(6,0)
+var grey = Vector2i(7,0)
+
 
 func _ready():
-	var coords = Vector2i(1,1)
-	var atlas_coords = Vector2i(1,0)
-	set_cell(coords, 0, atlas_coords, 0)
+	draw_line_at(2,2)
 	pass
 
+func draw_line_at(x, y):
+	for i in range(4):
+		var coords = Vector2i(x,y + i)
+		set_cell(coords, 0, yellow, 0)
+	
