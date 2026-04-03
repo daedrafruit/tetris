2889961038e77140850f77b8db2a505cdf442635
daedrafruit <160567370+daedrafruit@users.noreply.github.com>
2026-04-02 19:40:05 -0700

    got random shapes on random colors falling

--- tile_map_layer.gd
+++ tile_map_layer.gd
@@ -9,35 +9,100 @@ var orange = Vector2i(5,0)
 var purple = Vector2i(6,0)
 var grey = Vector2i(7,0)
 
+var O =\
+[\
+	0,0,0,0,\
+	0,1,1,0,\
+	0,1,1,0,\
+	0,0,0,0\
+]
+
+var I =\
+[\
+	0,0,0,0,\
+	1,1,1,1,\
+	0,0,0,0,\
+	0,0,0,0\
+]
+
+var S =\
+[\
+	0,1,0,0,\
+	0,1,1,0,\
+	0,0,1,0,\
+	0,0,0,0\
+]
+
+var Z =\
+[\
+	0,0,1,0,\
+	0,1,1,0,\
+	0,1,0,0,\
+	0,0,0,0\
+]
+
+var L =\
+[\
+	0,1,0,0,\
+	0,1,0,0,\
+	0,1,1,0,\
+	0,0,0,0\
+]
+
+var J =\
+[\
+	0,0,1,0,\
+	0,0,1,0,\
+	0,1,1,0,\
+	0,0,0,0\
+]
+var T =\
+[\
+	0,1,0,0,\
+	1,1,1,0,\
+	0,0,0,0,\
+	0,0,0,0\
+]
+
+
+var shape_colors = [yellow, sky, blue, red, green, orange, purple]
+var color
+
+var shapes = [O, I, S, Z, L, J, T]
+var shape
+
+var speed = 0
+var steps = 0
+var steps_req = 25
+
 var line_pos = 1
 
+var location = 1
+
 func _ready() -> void:
-	pass
+	new_game()
 
+func new_game():
+	speed = 1.0
+	shape = shapes[randi() % 4]
+	color = shape_colors[randi() % 7]
+	
 func _process(delta: float) -> void:
+	steps += speed
+	if steps >= steps_req:
+		clear_piece()
+		draw_shape_at(5, location, shape, color)
+		location += 1
+		steps = 0
 
-	var time_step = 0.5
-	var max_time_step = 0.25
-	var accumulator = 0.0
-
-	accumulator += max(delta, max_time_step)
-
-	if accumulator >= time_step:
-		clear_screen()
-
-		draw_line_at(2,line_pos)
-		line_pos += 1
-		accumulator -= time_step
-
-
-	pass
-
-func draw_line_at(x: int, y: int):
+func draw_shape_at(x: int, y: int, shape: Array, color: Vector2i):
 	for i in range(4):
-		var coords = Vector2i(x,y + i)
-		set_cell(coords, 0, yellow, 0)
+		for j in range(4):
+			if shape[j + i * 4]:
+				var coords = Vector2i(x + j, y + i)
+				set_cell(coords, 0, color, 0)
 
-func clear_screen() -> void:
+func clear_piece() -> void:
 	for i in range(1,11):
 		for j in range(1,20):
 			erase_cell(Vector2i(i,j))
