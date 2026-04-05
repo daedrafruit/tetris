9cf6cf92f046fb120c16737aa52670809aa6829d
daedrafruit <160567370+daedrafruit@users.noreply.github.com>
2026-04-05 15:34:03 -0700

    still trying to figure out collision

--- tile_map_layer.gd
+++ tile_map_layer.gd
@@ -9,10 +9,6 @@ var orange = Vector2i(5,0)
 var purple = Vector2i(6,0)
 var grey = Vector2i(7,0)
 
-var shape_colors = [yellow, sky, blue, red, green, orange, purple]
-var init_color
-
-
 var O = [
 	[0,0,0,0, 0,1,1,0, 0,1,1,0, 0,0,0,0]
 ]
@@ -53,10 +49,13 @@ var T = [
 	[0,1,0, 0,1,1, 0,1,0],
 ]
 
-var shapes = [O, I, S, Z, L, J, T]
-var active_shape
+var shape_colors = [yellow, sky, blue, red, green, orange, purple]
+var curr_color
 
-var shape_rotation = 0
+var shapes = [O, I, S, Z, L, J, T]
+var curr_shape
+
+var curr_rotation = 0
 
 var speed = 0
 var steps = 0
@@ -65,44 +64,61 @@ var steps_req = 25
 var line_pos = 1
 
 var location = 1
+var curr_location = Vector2i(5, 0)
 
 func _ready() -> void:
 	new_game()
 
 func new_game():
 	speed = 1.0
-	active_shape = shapes[randi() % 4][rotation]
-	init_color = shape_colors[randi() % 7]
+	curr_shape = shapes[randi() % 4][rotation]
+	curr_color = shape_colors[randi() % 7]
 	
 func _process(delta: float) -> void:
 	steps += speed
-	#var shape_rotation = randi() % init_shape.size()
+	#var curr_rotation = randi() % init_shape.size()
 	if steps >= steps_req:
-		if can_move(5, location, 0, 1):
+		if can_move(0, 0):
 			clear_screen()
-			draw_shape_at(5, location, init_color)
+			draw_shape()
 			steps = 0
-			location += 1
+			curr_location.y += 1
 
-func draw_shape_at(x: int, y: int, color: Vector2i):
-	var size = sqrt(active_shape.size())
-	for i in range(size):
-		for j in range(size):
-			if active_shape[j + i * size]:
-				var coords = Vector2i(x + j, y + i)
-				set_cell(coords, 0, color, 0)
+func draw_shape():
+	var size = sqrt(curr_shape.size())
+	for j in range(size):
+		for i in range(size):
+			if not curr_shape[i + j * size]:
+				continue
 
-func can_move(x: int, y:int, x_offset: int, y_offset: int):
-	var size = sqrt(active_shape.size())
-	for i in range(size):
-		for j in range(size):
-			var coords = Vector2i(x + j + x_offset, y + i + y_offset)
+			var coords = Vector2i(curr_location.x + i, curr_location.y + j)
+			set_cell(coords, 0, curr_color, 0)
+
+func can_move(x_offset: int, y_offset: int):
+	var size = sqrt(curr_shape.size())
+	var x = curr_location.x
+	var y = curr_location.y
+
+	for j in range(size):
+		for i in range(size):
+			if not curr_shape[i + j * size]:
+				continue
+
+			var x_rel = i + x_offset
+			var y_rel = j + y_offset
+			var within_shape = x_rel < size and y_rel < size
+
+			if within_shape and curr_shape[x_rel + y_rel * size]:
+				continue
+
+			var coords = Vector2i(x + i + x_offset, y + j + y_offset)
+			
 			if get_cell_source_id(coords) != -1:
 				return false
 	return true
 
 func clear_screen() -> void:
-	for i in range(1,11):
-		for j in range(1,20):
+	for j in range(1,20):
+		for i in range(1,11):
 			erase_cell(Vector2i(i,j))
 	
