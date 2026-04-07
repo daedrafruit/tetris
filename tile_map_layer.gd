606f33b4e65bc99489ed9d9d76a4448e2245ea43
daedrafruit <160567370+daedrafruit@users.noreply.github.com>
2026-04-07 16:09:01 -0700

    huge refactor shape->piece, experimenting with piece class

--- tile_map_layer.gd
+++ tile_map_layer.gd
@@ -49,102 +49,103 @@ var T = [
 	[0,1,0, 0,1,1, 0,1,0],
 ]
 
-var shape_colors = [yellow, sky, blue, red, green, orange, purple]
-var curr_color
+var piece_colors = [yellow, sky, blue, red, green, orange, purple]
 
-var shapes = [O, I, S, Z, L, J, T]
-var curr_shape
-var curr_shape_index
-
-var curr_rotation = 0
+var pieces = [O, I, S, Z, L, J, T]
 
 var speed = 0
 var steps = 0
 var steps_req = 25
 
-var line_pos = 1
 
-var location = 1
-var curr_location = Vector2i(4, 0)
+class piece:
+	var id: int = randi() % 7
+	var color_index: int = randi() % 7
+	var rotation: int = 0
+	var location: Vector2i = Vector2i(4, 0)
+
+var curr_piece: piece
 
 func _ready() -> void:
 	speed = 1.0
-	curr_shape_index = randi() % 7
-	curr_shape = shapes[curr_shape_index][rotation]
-	curr_color = shape_colors[randi() % 7]
+	curr_piece = piece.new()
 
 func _process(delta: float) -> void:
 	steps += speed
-	#var curr_rotation = randi() % init_shape.size()
+	#var curr_rotation = randi() % init_piece.size()
 	if steps >= steps_req:
-		if not move_piece(0, 1):
-			curr_location = Vector2i(4, 0)
-			curr_shape_index = randi() % 4
-			curr_shape = shapes[curr_shape_index][0]
-			curr_color = shape_colors[randi() % 7]
+		if not move_piece(curr_piece, 0, 1):
+			curr_piece = piece.new()
 		steps = 0
 
 func _input(event):
 	if event.is_action_pressed("left"):
-		move_piece(-1,0)
+		move_piece(curr_piece, -1,0)
 	if event.is_action_pressed("right"):
-		move_piece(1,0)
+		move_piece(curr_piece, 1,0)
 	if event.is_action_pressed("rotate clockwise"):
-		rotate_piece_clockwise()
+		rotate_piece_clockwise(curr_piece)
 
-func rotate_piece_clockwise():
-	if curr_rotation == shapes[curr_shape_index].size() - 1:
-		curr_rotation = 0
+func rotate_piece_clockwise(p: piece):
+	clear_piece(p)
+	if p.rotation == pieces[p.id].size() - 1:
+		p.rotation = 0
 	else:
-		curr_rotation += 1
-	clear_shape()
-	curr_shape = shapes[curr_shape_index][curr_rotation]
+		p.rotation += 1
+	draw_piece(p)
 
-func draw_shape():
-	var size = sqrt(curr_shape.size())
+func get_piece_array(p: piece) -> Array:
+	return pieces[p.id][p.rotation]
+
+func draw_piece(p: piece):
+	var curr_piece_array = get_piece_array(p)
+
+	var size = sqrt(curr_piece_array.size())
 	for j in range(size):
 		for i in range(size):
-			if not curr_shape[i + j * size]:
+			if not curr_piece_array[i + j * size]:
 				continue
 
-			var coords = Vector2i(curr_location.x + i, curr_location.y + j)
-			set_cell(coords, 0, curr_color, 0)
+			var coords = Vector2i(p.location.x + i, p.location.y + j)
+			set_cell(coords, 0, piece_colors[p.color_index], 0)
 
-func clear_shape():
-	var size = sqrt(curr_shape.size())
+func clear_piece(p: piece):
+	var curr_piece_array = get_piece_array(p)
+	var size = sqrt(curr_piece_array.size())
 	for j in range(size):
 		for i in range(size):
-			if not curr_shape[i + j * size]:
+			if not curr_piece_array[i + j * size]:
 				continue
 
-			var coords = Vector2i(curr_location.x + i, curr_location.y + j)
+			var coords = Vector2i(p.location.x + i, p.location.y + j)
 			erase_cell(coords)
 
-func move_piece(x_offset: int, y_offset: int) -> bool:
-	if can_move(x_offset, y_offset):
-		clear_shape()
-		curr_location.y += y_offset
-		curr_location.x += x_offset
-		draw_shape()
+func move_piece(p: piece, x_offset: int, y_offset: int) -> bool:
+	if can_move(p, x_offset, y_offset):
+		clear_piece(p)
+		p.location.y += y_offset
+		p.location.x += x_offset
+		draw_piece(p)
 		return true
 	else:
 		return false
 			
-func can_move(x_offset: int, y_offset: int):
-	var size = sqrt(curr_shape.size())
+func can_move(p: piece, x_offset: int, y_offset: int):
+	var curr_piece_array = get_piece_array(p)
+	var size = sqrt(curr_piece_array.size())
 	for j in range(size):
 		for i in range(size):
-			if not curr_shape[i + j * size]:
+			if not curr_piece_array[i + j * size]:
 				continue
 
 			var x_rel = i + x_offset
 			var y_rel = j + y_offset
-			var within_shape = x_rel < size and y_rel < size
+			var within_piece = x_rel < size and y_rel < size
 
-			if within_shape and curr_shape[x_rel + y_rel * size]:
+			if within_piece and curr_piece_array[x_rel + y_rel * size]:
 				continue
 
-			var coords = Vector2i(curr_location.x + x_rel, curr_location.y + y_rel)
+			var coords = Vector2i(p.location.x + x_rel, p.location.y + y_rel)
 			
 			if get_cell_source_id(coords) != -1:
 				return false
