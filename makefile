3ebf6b91514b284994832ffd265a2e2fcd2a114f
daedrafruit <160567370+daedrafruit@users.noreply.github.com>
2026-04-25 22:17:02 -0700

    more experiments

--- makefile
+++ makefile
@@ -2,7 +2,11 @@ make: clean build run
 
 build: src/tetris.c
 	#gcc -g -o tetris tetris.c 
-	gcc -g -Iinclude -lSDL3 -lglfw -o tetris src/render.c
+	gcc -g \
+		-Iinclude \
+		-lSDL3 -lglfw \
+		-o tetris \
+		src/glad.c src/render.c
 
 run: build
 	./tetris
--- src/render.c
+++ src/render.c
@@ -1,6 +1,14 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 #include <glad/glad.h>
+#include <stdio.h>
+
+const char *vertexShaderSource = "#version 330 core\n"
+  "layout (location = 0) in vec3 aPos;\n"
+  "void main()\n"
+  "{\n"
+  " gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+  "}\0";
 
 int main(int argc, char* argv[]) {
 
@@ -40,8 +48,43 @@ int main(int argc, char* argv[]) {
           -0.5f, -0.5f, 0.0f
         };
 
+        // vertex buffer object
         unsigned int VBO;
+        // generate vbo
         glGenBuffers(1, &VBO);
+        // bind vbo 
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        // copy vertex data to vbo
+        glBufferData(
+          GL_ARRAY_BUFFER, //buffer
+          sizeof(vertices), //size
+          vertices, //vertices
+          GL_STATIC_DRAW //method (static is for vertices that wont change often, something like dynamic would be good for often changing ones)
+        );
+
+        //create vertex shader
+        unsigned int vertexShader;
+        vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+        //attach source code (top of file)
+        glShaderSource(
+          vertexShader, //shader object
+          1, //number of strings 
+          &vertexShaderSource, //shader source code
+          NULL 
+        );
+        //compile shader
+        glCompileShader(vertexShader);
+
+        int success;
+        char infoLog[512];
+        glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+        if(!success) {
+          glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+          printf("ERROR::SHADER::VERTEX::COMPILATION_FAILED\n");
+          printf("%s", infoLog) ;
+        }
+
 
     }
 
